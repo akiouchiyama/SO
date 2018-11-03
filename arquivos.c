6e4560393307231e6996583fa8da946312c8472a
arthurghz01 <a213281@g.unicamp.br>
2018-11-03 10:28:40 -0300

    Atualizar

---
+++ arquivos.c
@@ -0,0 +1,12 @@
+#include <arquivos.h>
+#include <stdio.h>
+
+void ler_vetoriza_arquivo(arqv[])   
+{       
+    int tam =sizeof(arqv[]/sizeof(int));
+    for(int i=1; i<=tam;i++)
+        fopen(arqv[i],r); //abre o arquivo para ler.
+        fopen(arq[tam],a);
+    fscanf(arq[i],"%d",arq[tam]);
+}
+teste12
