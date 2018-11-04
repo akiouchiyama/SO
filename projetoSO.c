74f31f4084100a61d39546e3ce724cadc5c229e3
arthurghz01 <a213281@g.unicamp.br>
2018-11-03 21:25:47 -0300

    Ok!

--- projetoSO.c
+++ projetoSO.c
@@ -6,23 +6,22 @@
 
 int main(int arqc,char* arqv[])
 {       
-   
    //ler_vetoriza_arquivo(arqc, arqv);
     FILE *arq2; //Define arquivo que irá ser escrito        
     arq2=fopen(arqv[arqc-1],"w");//cria o arquivo a ser escrito
+
     for(int i=1; i<arqc;i++) //Laço para ver os arquivos de entrada
     {   
         FILE *arq1; //Arquivo de entrada
         arq1=fopen(arqv[i],"r"); //abre o arquivo para leitura
-        do
+      
+        while(!feof(arq1)) //Enquanto não encontra o fim do arquivo
         {
         int numero; 
-        fscanf(arq1,"%d\n",&numero); //lê do arquivo e salva em numero
-        fprintf(arq2,"%d\n",numero); //lê numero e salva no arquivo novo        
-        }while(!feof(arq1));//Enquanto não encontra o fim do arquivo
-
+        fscanf(arq1,"%d",&numero); //lê do arquivo e salva em numero
+        fprintf(arq2,"%d ",numero); //lê numero e salva no arquivo novo
+        }
         fclose(arq1);//fecha o arquivo de leitura.
-       
     }
 
         return 0;
