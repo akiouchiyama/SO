09fd850ae44c8ff35283b50c95e0470514598be1
arthurghz01 <a213281@g.unicamp.br>
2018-11-03 22:11:54 -0300

    OK - 1/3 (Problema era o FOR)

--- projetoSO.c
+++ projetoSO.c
@@ -6,23 +6,8 @@
 
 int main(int arqc,char* arqv[])
 {       
-   //ler_vetoriza_arquivo(arqc, arqv);
-    FILE *arq2; //Define arquivo que irá ser escrito        
-    arq2=fopen(arqv[arqc-1],"w");//cria o arquivo a ser escrito
-    printf("%d",arqc);
-    int numero;
-    for(int i=1; i<arqc-1;i++) //Laço para ver os arquivos de entrada
-    {   
-        FILE *arq1; //Arquivo de entrada
-        arq1=fopen(arqv[i],"r"); //abre o arquivo para leitura
-      
-        while(!feof(arq1)) //Enquanto não encontra o fim do arquivo
-        { 
-        fscanf(arq1,"%d",&numero); //lê do arquivo e salva em numero
-        fprintf(arq2,"%d ",numero); //lê numero e salva no arquivo novo
-        }
-        fclose(arq1);//fecha o arquivo de leitura.
-    }
+   ler_vetoriza_arquivo(arqc, arqv);
+   
 
         return 0;
 
