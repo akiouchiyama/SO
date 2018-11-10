3d470f12c70e1027dd44a3a4eab5839c07a5f95e
arthurghz01 <a213281@g.unicamp.br>
2018-11-10 00:16:50 -0200

    Com - Insertion Sort  - Threads

--- projetoSO.c
+++ projetoSO.c
@@ -4,19 +4,25 @@
 #include <stdlib.h>
 #include <pthread.h>
     
-int k=0; //Contador para saber quantos inteiros tem o arquivo de saída.
+int k=2; //Contador para saber quantos inteiros tem o arquivo de saída.
 int j=1000; //Variavel para aumentar de 1000 em 1000 a memoria alocada.
 int *num; //Memoria alocada dinamicamente.
-/* -------------------------------------------------
-	     DECLARA TODAS AS FUNÇÕES ANTES DA MAIN
-   ------------------------------------------------- */
+int y=2; //Auxiliar para o vetor
+/* ------------------------------------------------------------------
+	     PROTOTIPO DE TODAS AS FUNÇÕES USADAS NO PROGRAMA 
+   ------------------------------------------------------------------ */
+ typedef struct{  // STRUCT PARA PASSAR OS VALORES PARA A THREAD.
+        int *auxnum; //recebe INICIO VETOR
+        int auxf; //recebe FINAL vetor
+    }thread_arg, *ptr_thread_arg; 
+
 
 void gerar_arquivo(int arqc, char* arqv[])    //FUNÇÃO GERA ARQUIVO .OUT
 {        
     FILE *arq2; //Define arquivo que irá ser escrito        
     arq2=fopen(arqv[arqc-1],"w");//cria o arquivo a ser escrito
     int numero;
-    for(int i=1; i<arqc-1;i++) //Laço para ver os arquivos de entrada
+    for(int i=2; i<arqc-1;i++) //Laço para ver os arquivos de entrada
     {   
         FILE *arq1; //Arquivo de entrada
         arq1=fopen(arqv[i],"r"); //abre o arquivo para leitura
@@ -41,8 +47,7 @@ void vetoriza(int arqc, char* arqv[]){    // FUNÇÃO COLOCA TODOS OS DADOS DO A
 
     while(!feof(arq)){
          fscanf(arq,"%d\n",&num[k]); //Lê todos os números do arquivo e salva em num.
-         k++; //soma k para verificar quantos números(inteiros) possui o arquivo
-         
+           k++; //soma k para verificar quantos números(inteiros) possui o arquivo
          if(k==j) //k chegou ao limite da memoria alocada 
          {
            j=k+1000; //J recebe K+1000 para alocar K+1000
@@ -54,62 +59,76 @@ void vetoriza(int arqc, char* arqv[]){    // FUNÇÃO COLOCA TODOS OS DADOS DO A
 
 }
 
-void quick_sort(int *a, int ini, int fim) {  //QUICK_SORT PARA ORDENA O VETOR
-    int i, j, x, y;
-    i = ini;
-    j = fim;
-    x = a[(ini + fim) / 2];
-     
-    while(i <= j) {
-        while(a[i] < x && i < fim) {
-            i++;
-        }
-        while(a[j] > x && j > ini) {
-            j--;
-        }
-        if(i <= j) {
-            y = a[i];
-            a[i] = a[j];
-            a[j] = y;
-            i++;
-            j--;
-        }
+void *ordenacao (void *arg){ //algoritmo de ordenação - insertion short que será chamado pela thread
+
+    ptr_thread_arg targ = (ptr_thread_arg)arg; //CARREGA A ESTRUTURA NA FUNÇÃO
+    int i, j, x;
+    for (i=y; i<=targ->auxf;i++){ //Y É O INICIO DO VETOR
+    x = targ->auxnum[i];
+    j=i-1;
+    targ->auxnum[1] = x; 
+    while (x < targ->auxnum[j]){
+        targ->auxnum[j+1] = targ->auxnum[j];
+        j--;
     }
-    if(j > ini) {
-        quick_sort(a, ini, j);
-    }
-    if(i < fim) {
-        quick_sort(a, i, fim);
-    }
+    targ->auxnum[j+1] = x;
+    }  
 }
 
 void escrita(int arqc, char* arqv[]){
 
     FILE *arq; //Define arquivo que irá ser escrito.
     arq=fopen(arqv[arqc-1],"w");//Abre o arquivo a ser lido.
-    for(int i=1;i<k;i++) //Escreve inteiro por inteiro no arquivo
-      fprintf(arq,"%d ",num[i]); 
+    for(int i=2;i<k && num>-1;i++){ //Escreve inteiro por inteiro no arquivo
+      fprintf(arq,"%d ",num[i]);
+    } 
     fclose(arq);//fecha arquivo após escreve tudo
 }
 
+void thread(int T){ //Divide a quantidade de numeros de entrada pela qtd de threads
+   time_t segundos = time(NULL);
+   
+   
+    pthread_t thread[T]; //CRIA Números de Threads pedidas pelo usuário
+    thread_arg arguments[T]; 
+   
+    for(int j=0; j<T;j++){        
+        arguments[j].auxnum=num; //Recebe o vetor
+        int a=j+1; //Variavel aux para aumentar o fim do vetor até o final da lista
+        arguments[j].auxf=(((k-1)/T)*a)+ (k-1)%T; // Delimita o fim do vetor para cada Thread
+        pthread_create(&thread[j],NULL, ordenacao,&(arguments[j]));
+        pthread_join(thread[j],NULL); 
+        y=arguments[j].auxf; //O FINAL do vetor passa a ser o começo a cada nova thread
+        
+    }
+
+    float tempo_decorrido = time(NULL) - segundos;
+
+    printf("%f",tempo_decorrido);
+}
 /* -------------------------------------------------
 			    INICIO DA FUNÇÃO MAIN 
    ------------------------------------------------- */
-
     int main(int arqc,char* arqv[])
 {  
-    int T = atoi (arqv[1]);//RECEBE NUMERO DE THREADS DO USUÁRIO
-
+      int T = atoi (arqv[1]);//Numero de threads inseridas pelo Usuario
+    
+     if(T!=2 && T!=4 && T!=8 && T!=16){ //Se o número de threads diferente de 2,4,6,8 - SAI DO PROGRAMA
+        printf("Numero de Threads Invalido");
+        exit(1); //SAI DO PROGRAMA
+    }
 
    gerar_arquivo(arqc, arqv); //Chama função geradora do arquivo de saida sem ordenação.
    vetoriza(arqc,arqv); //Função para ler o arquivo e salva os dados no vetor num
-   quick_sort(num,0 , k); //Ordena o vetor
-   escrita(arqc,arqv);
 
-    
-     //ESCREVER ALGORITMO DE ORDENAÇÃO ANTES DE LIBERA MEMORIA  
-    free(num);//libera memoria7
+  
+   thread(T);  //Carrega numero de Threads inseridas e começa a ordenada.
+   escrita(arqc,arqv);
+   free(num);//libera memoria7
+
 
         return 0;
 
+
 }
+
